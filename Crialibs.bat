4441807a1fbd76dcbc2aa9aaac0179580006c1bf
Ivanil Marcelino <ivanil.marcelino@yahoo.com.br>
2026-03-04 20:49:17 -0300

    Preparando compilações independentes;
     Changes to be committed:
    	modified:   Crialibs.bat
    	modified:   Include/hmg.hbx
    	new file:   hbmk/Designer.hbc.txt
    	new file:   hbmk/GraphPlus.hbp
    	new file:   hbmk/HBCrypto.hbx
    	new file:   hbmk/HbComm.hbp
    	new file:   hbmk/adordd.hbp
    	new file:   hbmk/bostaurus.hbp
    	new file:   hbmk/calldll.hbp
    	new file:   hbmk/debugger.hbp
    	new file:   hbmk/dll.hbp
    	new file:   hbmk/hbaes.hbp
    	new file:   hbmk/hbcab.hbp
    	new file:   hbmk/hbcrypto.hbp
    	new file:   hbmk/hmg.hbp
    	new file:   hbmk/list.txt

--- Crialibs.bat
+++ Crialibs.bat
@@ -1,35 +1,37 @@
-;Under construction;
+::Under construction;
 
 @echo off
 @CHCP 1252 >NUL
 CD "%~dp0%"
 @SetLocal
 
-;Path do Harbour
+::Path do Harbour
 Set MG_CMP=MG_CMP
 Set MG_HRB=D:\prgplus\Youtube\IDE\Tools\Harbour
 
-;path do Borland
+::path do Borland
 Set MG_BCC=D:\prgplus\Youtube\IDE\Tools\BCC
 
-;Path da Minigui
+::Path da Minigui
 Set MG_ROOT=%~dp0%
 Set MG_LIB=%MG_ROOT%lib
 Set MG_XLIB=%MG_ROOT%xlib
 
-set Path=%MG_CMP%\bin;%MG_BCC%\bin;%path%
-set Include=%MG_CMP%\include;%MG_ROOT%include;%MG_BCC%\include;%MG_BCC%\include\windows\crtl;%MG_BCC%\include\windows\rtl;%MG_BCC%\include\windows\sdk
+set Path=%MG_CMP%\bin;%MG_BCC%\bin;%MG_HRB%\bin;%path%
+set Include=%MG_HRB%\include;%MG_CMP%\include;%MG_ROOT%include;%MG_BCC%\include;%MG_BCC%\include\windows\crtl;%MG_BCC%\include\windows\rtl;%MG_BCC%\include\windows\sdk
 
-echo %MG_ROOT%
-echo %mg_lib%
-echo %mg_cmp%
-echo %mg_hrb%
-echo %mg_bcc%
-echo %include%
-pause
-
-CD SOURCE
-CALL MakeAllLibs.bat 
+CD hbmk
+CALL hbmk2 hmg.hbp
+CALL hbmk2 adordd.hbp
+CALL hbmk2 bostaurus.hbp
+Call hbmk2 calldll.hbp
+Call hbmk2 debugger.hbp
+Call hbmk2 dll.hbp
+call hbmk2 GraphPlus.hbp
+Call hbmk2 hbaes.hbp
+Call hbmk2 hbcab.hbp
+Call hbmk2 hbComm.hbp
+Call hbmk2 HBCrypto.hbp
 pause
 
 echo %MG_CMP%
